2427387e92576f7ab5162d32a99c00700e48f3f6
Kostyantyn Proskuryakov <2399977+KProskuryakov@users.noreply.github.com>
2023-02-02 13:51:38 -0500

    17 part 1

--- src/2022/day17/day17.spec.ts
+++ src/2022/day17/day17.spec.ts
@@ -5,15 +5,15 @@ import { part1, part2 } from "./day17";
 describe("2022-17", () => {
   describe("part 1", () => {
     it("does the thing with the small input", () => {
-      expect(part1(linesFromFile("src/2022/day17/smallinput.txt"))).to.equal(0);
+      expect(part1(linesFromFile("src/2022/day17/smallinput.txt"))).to.equal(3068);
     });
     it("solves the puzzle", () => {
-      expect(part1(linesFromFile("src/2022/day17/input.txt"))).to.equal(0);
+      expect(part1(linesFromFile("src/2022/day17/input.txt"))).to.equal(3151);
     });
   });
   describe("part 2", () => {
     it("does the thing with the small input", () => {
-        expect(part2(linesFromFile("src/2022/day17/smallinput.txt"))).to.equal(0);
+        expect(part2(linesFromFile("src/2022/day17/smallinput.txt"))).to.equal(1514285714288);
       });
     it("solves the puzzle", () => {
       expect(part2(linesFromFile("src/2022/day17/input.txt"))).to.equal(0);
--- src/2022/day17/day17.ts
+++ src/2022/day17/day17.ts
@@ -1,7 +1,145 @@
-export function part1(lines: string[]) {
-  return lines.length;
+type Shape = number[][];
+
+const bar = [[1, 1, 1, 1]]
+const plus = [
+  [0, 1, 0],
+  [1, 1, 1],
+  [0, 1, 0]
+]
+const ell = [
+  [0, 0, 1],
+  [0, 0, 1],
+  [1, 1, 1]
+]
+const line = [
+  [1],
+  [1],
+  [1],
+  [1]
+]
+const square = [
+  [1, 1],
+  [1, 1]
+]
+
+const order = [bar, plus, ell, line, square];
+
+export function part1(lines: string[], pieces = 2022) {
+  const moves = Array.from(lines[0]).map(c => c === ">" ? 1 : -1);
+
+  const board = new Set<number>();
+  let curHeight = 0;
+  let curMove = 0;
+
+  for (let i = 0; i < pieces; i++) {
+    const shape = order[i % 5];
+    let moving = true;
+    let x = 2;
+    let y = curHeight + 3;
+    while (moving) {
+      const move = moves[curMove];
+      curMove++;
+      curMove = curMove % moves.length;
+      if (!collides(x + move, y, shape, board)) {
+        x += move;
+      }
+      if (!collides(x, y - 1, shape, board)) {
+        y -= 1;
+      } else {
+        fix(x, y, shape, board);
+        moving = false;
+        curHeight = Math.max(curHeight, y + shape.length);
+      }
+    }
+  }
+  return curHeight;
 }
 
-export function part2(lines: string[]) {
-  return lines.length;
+function collides(x: number, y: number, shape: Shape, board: Set<number>) {
+  if (x < 0 || x + shape[0].length > 7) {
+    return true;
+  }
+  if (y < 0) {
+    return true;
+  }
+  for (let i = 0; i < shape.length; i++) {
+    for (let j = 0; j < shape[0].length; j++) {
+      if (shape[i][j] === 1) { // check square
+        if (board.has((y + shape.length - i - 1) * 7 + (x + j))) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
+
+function fix(x: number, y: number, shape: Shape, board: Set<number>) {
+  for (let i = 0; i < shape.length; i++) {
+    for (let j = 0; j < shape[0].length; j++) {
+      if (shape[i][j] === 1) { // check square
+        board.add((y + shape.length - i - 1) * 7 + (x + j));
+      }
+    }
+  }
+}
+
+export function part2(lines: string[], pieces = 1000000000000) {
+  const moves = Array.from(lines[0]).map(c => c === ">" ? 1 : -1);
+
+  const board = new Set<number>();
+  let curHeight = 0;
+  let curMove = 0;
+
+  for (let i = 0; i < pieces; i++) {
+    const shape = order[i % 5];
+    let moving = true;
+    let x = 2;
+    let y = curHeight + 3;
+    while (moving) {
+      const move = moves[curMove];
+      curMove++;
+      curMove = curMove % moves.length;
+      if (!collides2(x + move, y, shape, board)) {
+        x += move;
+      }
+      if (!collides2(x, y - 1, shape, board)) {
+        y -= 1;
+      } else {
+        fix2(x, y, shape, board);
+        moving = false;
+        curHeight = Math.max(curHeight, y + shape.length);
+      }
+    }
+  }
+  return curHeight;
+}
+
+function collides2(x: number, y: number, shape: Shape, board: Set<number>) {
+  if (x < 0 || x + shape[0].length > 7) {
+    return true;
+  }
+  if (y < 0) {
+    return true;
+  }
+  for (let i = 0; i < shape.length; i++) {
+    for (let j = 0; j < shape[0].length; j++) {
+      if (shape[i][j] === 1) { // check square
+        if (board.has((y + shape.length - i - 1) * 7 + (x + j))) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
+
+function fix2(x: number, y: number, shape: Shape, board: Set<number>) {
+  for (let i = 0; i < shape.length; i++) {
+    for (let j = 0; j < shape[0].length; j++) {
+      if (shape[i][j] === 1) { // check square
+        board.add((y + shape.length - i - 1) * 7 + (x + j));
+      }
+    }
+  }
 }
